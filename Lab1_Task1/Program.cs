5be7d57f425975335ed58b09c8c5e7ce2cd1f32d
Semonic61 <91585111+Semonic61@users.noreply.github.com>
2023-01-19 23:37:30 +0800

    Добавьте файлы проекта.

---
+++ Lab1_Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите двузначное число: ");
+            string s = Console.ReadLine();
+            Console.Write("Введите двузначное число: ");
+            string a = Console.ReadLine();
+            int i = 0, j = 1;
+            while (i < s.Length && j < s.Length)
+            {
+                bool v = s[j] == a;
+                bool v1 = s[i] == a;
+                if (v1 || v)
+                {
+                    Console.WriteLine("Число содержит цифру", a);
+                }
+                else
+                {
+                    Console.WriteLine("Число не содержит цифру", a);
+                }
+                break;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
